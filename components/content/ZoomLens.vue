a2e12a889aac8fd893ba85f9caf29a95c15bbd95
Najia25 <najiaafrin2@gmail.com>
2021-01-18 15:15:38 +0600

    product image zoom

---
+++ components/content/ZoomLens.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='zoom-lens'>
+    dfdsfds
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+.zoom-lens{
+  position: relative;
+  height: 648px;
+  width: 519px;
+}
+</style>
--- pages/product/_slug.vue
+++ pages/product/_slug.vue
@@ -3,48 +3,60 @@
     <div v-if="$apollo.queries.product.loading">...Loading</div>
 
     <v-row v-else>
-      <div class="col-md-8 col-xl-6 col-12 offset-xl-1">
-        <div class="d-flex flex-column-reverse flex-md-row">
-          <v-tabs
-            v-model="tab"
-            hide-slider
-            :vertical="$vuetify.breakpoint.mdAndUp"
-            class="mr-md-5 mt-5 mt-md-0"
-            :height="$vuetify.breakpoint.mdAndUp ? undefined : 60"
-          >
-            <v-tab
-              v-for="item in productGellaryImages"
-              :key="item.sourceUrl"
-              class="px-0 mb-5 mr-2 mr-md-0"
+      <div class="col-12">
+        <div class="d-flex flex-column flex-md-row align-sm-center align-md-start">
+          <div class="d-flex flex-column-reverse flex-md-row mr-md-5">
+            <v-tabs
+              v-model="tab"
+              hide-slider
+              :vertical="$vuetify.breakpoint.mdAndUp"
+              class="mr-md-5 mt-5 mt-md-0"
+              :height="$vuetify.breakpoint.mdAndUp ? undefined : 60"
             >
-              <v-card
-                flat
-                rounded
-                link
+              <v-tab
+                v-for="item in productGellaryImages"
+                :key="item.sourceUrl"
+                class="px-0 mb-5 mr-2 mr-md-0"
               >
-                <v-img
-                  :src="item.sourceUrl"
-                  max-height="60"
-                  max-width="60"
+                <v-card
+                  flat
+                  rounded
+                  link
                 >
-                </v-img>
-              </v-card>
-            </v-tab>
-          </v-tabs>
+                  <v-img
+                    :src="item.sourceUrl"
+                    max-height="60"
+                    max-width="60"
+                  >
+                  </v-img>
+                </v-card>
+              </v-tab>
+            </v-tabs>
 
-          <v-tabs-items v-model="tab">
-            <v-tab-item
-              v-for="item in productGellaryImages"
-              :key="item.sourceUrl"
-              transition="none"
-              reverse-transition="none"
-            >
-              <v-card flat>
-              <v-img
-              :src="item.sourceUrl"></v-img>
-              </v-card>
-            </v-tab-item>
-          </v-tabs-items>
+            <v-tabs-items v-model="tab">
+              <v-tab-item
+                v-for="item in productGellaryImages"
+                :key="item.sourceUrl"
+                transition="none"
+                reverse-transition="none"
+              >
+                <v-card
+                  flat
+                  max-width="500"
+                >
+                  <v-img
+                  :src="item.sourceUrl"
+                  @mouseover="hover = true"
+                  @mouseleave="hover = false"
+                  class="product-img"
+                  >
+                    <div class="lens" v-if="hover" :style="{ transform: `translate(${xPos}px, ${yPos}px)`}"></div>
+                  </v-img>
+                </v-card>
+              </v-tab-item>
+            </v-tabs-items>
+          </div>
+          <zoom-lens v-if="hover"></zoom-lens>
         </div>
       </div>
     </v-row>
@@ -53,14 +65,21 @@
 
 <script>
 import gql from 'graphql-tag'
+import ZoomLens from '@/components/content/ZoomLens'
 export default {
+  components: {
+    ZoomLens
+  },
   data () {
     return {
       id: this.$route.params.slug.split('-').pop(),
       product: null,
       productGellaryImages: [],
       tab: null,
-      length: 0
+      length: 0,
+      hover: false,
+      xPos: 0,
+      yPos: 0
     }
   },
   watch: {
@@ -121,6 +140,20 @@ export default {
 }
 .v-tabs--vertical > .v-tabs-bar .v-tab {
   height: 60px;
+}
+
+.product-img {
+  position: relative;
+  cursor: move;
+}
+
+.lens {
+  position: absolute;
+  height: 127px;
+  width: 100px;
+  background: rgba(124, 181, 237, 0.52);
+  top:0;
+  left:0;
 }
 
 </style>
